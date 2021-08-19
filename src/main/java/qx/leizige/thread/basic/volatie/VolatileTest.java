0c05ef1599d3496927ca0a636773afac74b3a208
leizige <lei.tl@bitsun-inc.com>
2021-08-19 15:21:23 +0800

    volatile 使用

--- src/main/java/qx/leizige/thread/basic/volatie/VolatileTest.java
+++ src/main/java/qx/leizige/thread/basic/volatie/VolatileTest.java
@@ -1,29 +1,37 @@
 package qx.leizige.thread.basic.volatie;
 
-import java.util.concurrent.TimeUnit;
-
 /**
  * @author leizige
  * created 2021/08/09
+ * volatile解决共享变量value的内存可见性问题
  */
 public class VolatileTest {
 
+    /**
+     * 作用
+     * <p>
+     * 防止JVM对 Long/Double 等64位的非原子性协议进行的 误操作（读取半个数据）;
+     * 可以使某一个变量对所有的线程立即可见（某一个线程如果修改了工作内存中的变量副本，那么加上Volatile关键字之后，该变量就会立即同步到其他线程的工作内存当中）。
+     * 禁止指令 "重排序" 优化。
+     * </p>
+     */
+    private static volatile Integer num = 0;
 
     public static void main(String[] args) throws InterruptedException {
 
-        ThreadNotSafeInteger t = new ThreadNotSafeInteger();
-        for (int i = 0; i < 1000; i++) {
-            new Thread(() -> {
-                for (int j = 0; j < 1000; j++) {
-                    t.increment();
-                }
-            }).start();
-        }
+        new Thread(() -> num = 1, "A").start();
+        new Thread(() -> num = 10, "B").start();
+        new Thread(() -> num = 30, "C").start();
 
-        TimeUnit.SECONDS.sleep(3);
+        new Thread(() -> {
+            System.out.println(Thread.currentThread().getName() + "num value = " + num);
+        }, "D").start();
 
-        System.err.println(t.getValue());
+        new Thread(() -> num = 50, "E").start();
 
+        Thread.sleep(1000);
+
+        System.out.println("lasted num value = " + num);
     }
 
 }
