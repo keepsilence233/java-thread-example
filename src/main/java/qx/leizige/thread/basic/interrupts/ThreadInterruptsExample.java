99c50cbcacc6fd19b263381bad36152d594d36a5
“leizige” <qx_leizige@163.com>
2022-06-03 22:21:11 +0800

    Thread#interrupts example

--- src/main/java/qx/leizige/thread/basic/interrupts/ThreadInterruptsExample.java
+++ src/main/java/qx/leizige/thread/basic/interrupts/ThreadInterruptsExample.java
@@ -11,7 +11,13 @@ import java.util.concurrent.TimeUnit;
  *     中断是操作系统级别的机制，用于向线程发送有关特定事件的信号。
  *     在被中断的线程内，Thread.currentThread().isInterrupted() 可以用来查找被中断的状态。
  *     如果线程被阻塞 Object.wait(),Thread.join()或者Thread.sleep(),将抛出 InterruptedException。
- *
+ * </p>
+ * <p>
+ *     interrupt() : 中断线程，线程A运行时，线程B可以调用线程A的interrupt来设置A的中断标志为true并返回
+ *    					 设置标志仅仅是设置标志，线程A实际并没有被中断，它会继续往下执行。
+ *     isInterrupted() : 检测当前线程是否被中断，如果是返回true，否则返回false
+ *     interrupted() : 检测当前线程是否被中断，如果是返回true，否则返回false
+ *     					与isInterrupted不同的是，该方法如果发现当前线程被中断，则会清除中断标志，并且该方法是static方法，可以通过Thread类直接调用。
  * </p>
  */
 public class ThreadInterruptsExample {
@@ -20,8 +26,8 @@ public class ThreadInterruptsExample {
 		Task task1 = new Task();
 		Thread thread1 = new Thread(task1);
 		thread1.start();
-		while (true){
-			if(Math.random() > 0.5){
+		while (true) {
+			if (Math.random() > 0.5) {
 				thread1.interrupt();
 				break;
 			}
@@ -30,16 +36,16 @@ public class ThreadInterruptsExample {
 	}
 
 
-	private static class Task implements Runnable{
+	private static class Task implements Runnable {
 
 		@Override
 		public void run() {
 			int c = 0;
 
-			while (true){
+			while (true) {
 
 				System.out.println("task running .. " + ++c);
-				if(Thread.currentThread().isInterrupted()){
+				if (Thread.currentThread().isInterrupted()) {
 					System.out.println("interrupted flag=true");
 					terminate();
 					return;
@@ -55,7 +61,7 @@ public class ThreadInterruptsExample {
 			}
 		}
 
-		private void terminate () {
+		private void terminate() {
 			System.out.println("Terminating task");
 		}
 	}
